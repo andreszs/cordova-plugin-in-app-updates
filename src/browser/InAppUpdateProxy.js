09cb05c0cbd7217aaf3f749678c416cc41cd00c6
andreszs <10298071+andreszs@users.noreply.github.com>
2022-05-17 11:08:56 -0300

    2.1.0

---
+++ src/browser/InAppUpdateProxy.js
@@ -0,0 +1,20 @@
+"use strict";
+
+var InAppUpdate = {
+	getUpdateAvailability: function (successCallback, failureCallback) {
+		successCallback('UPDATE_NOT_AVAILABLE');
+	},
+	setSnackbarOptions: function (successCallback, failureCallback, snackbarText, snackbarButton, snackbarButtonColor) {
+		successCallback('SUCCESS');
+	},
+	updateFlexible: function (successCallback, failureCallback) {
+		successCallback('UPDATE_NOT_AVAILABLE');
+	},
+	updateImmediate: function (successCallback, failureCallback) {
+		successCallback('UPDATE_NOT_AVAILABLE');
+	}
+};
+
+module.exports = InAppUpdate;
+
+require("cordova/exec/proxy").add("InAppUpdate", module.exports);
