57773861f422b976de1a35b0fd372d96ab6c6d11
Junker <1144095+Junker@users.noreply.github.com>
2023-03-12 19:26:38 +0700

    remove deprecated 'com.google.android.play:core' https://developer.android.com/guide/playcore

--- src/android/InAppUpdate.java
+++ src/android/InAppUpdate.java
@@ -19,7 +19,7 @@ import com.google.android.play.core.install.model.UpdateAvailability;
 import com.google.android.play.core.install.model.InstallStatus;
 import com.google.android.play.core.install.InstallState;
 import com.google.android.play.core.install.InstallStateUpdatedListener;
-import com.google.android.play.core.tasks.Task;
+import com.google.android.gms.tasks.Task;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaInterface;
